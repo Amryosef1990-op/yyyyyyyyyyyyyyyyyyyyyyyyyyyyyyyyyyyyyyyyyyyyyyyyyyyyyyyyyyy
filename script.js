00d2a69a41c6bdc33332324889e2b213032df482
Amryosef1990-op <yassinyosef1990@gmail.com>
2025-09-09 05:55:47 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+function A()
+{
+alert("what did you learn about wolfs?")
+
+}
+function B()
+{
+window.location.href="page(2).html"
+
+}
+function e()
+{
+alert("What did you learn about pinguins")
+
+}
+function g()
+{
+window.location.href="page(3).html"
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
